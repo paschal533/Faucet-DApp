f5686f9c1c39f86715637ca47be312e815e9be49
unknown <okwuosahpaschal@gmail.com>
2022-01-15 12:16:32 +0100

    third commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "bulma/css/bulma.min.css";
-import './index.css';
-import App from './App';
+//import "bulma/css/bulma.min.css";
+//import './index.css';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
